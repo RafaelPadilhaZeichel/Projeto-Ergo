4a4fdd5e631598010849c53a49c3beaf7924af57
Rafael Padilha <padilharafael17@gmail.com>
2026-01-09 09:57:17 -0300

    Correção de caminhos e config do Vite

---
+++ vite.config.ts
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // IMPORTANTE: Troque 'Projeto-Ergo' pelo nome EXATO do seu repositório no GitHub
+  base: '/Projeto-Ergo/', 
+})
